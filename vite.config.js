4b613cce76a5ceb1eaab089ce2a10c49aa8fc14d
isaacxd <is.ascanoa@gmail.com>
2022-10-31 13:14:06 -0500

    FIX Bug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //Link de mi Pagina de GitHub
+  base: 'https://isaac244.github.io/',
   plugins: [react()]
 })
